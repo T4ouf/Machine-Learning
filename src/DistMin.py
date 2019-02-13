bb3672ba075f31e336464cc4ac5812d9ccc339b7
Thomas Von Ascheberg <tvonasc@a-018818.ups.u-psud.fr>
2019-02-13 10:30:29 +0100

    DistMin done

--- src/DistMin.py
+++ src/DistMin.py
@@ -1,2 +1,65 @@
 # -*- coding: utf-8 -*-
 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+training = np.load('../data/trn_img.npy')
+trainingLabel = np.load('../data/trn_lbl.npy')
+
+dev = np.load('../data/dev_img.npy')
+devLabel = np.load('../data/dev_lbl.npy')
+
+
+"""
+Function that prints an image (reshape is in the function)
+"""
+def printImage(im):
+    img = im.reshape(28,28)
+    plt.imshow(img,plt.cm.gray)
+    plt.show()
+
+
+# Defining our classes
+class0 = training[trainingLabel == 0]
+class1 = training[trainingLabel == 1]
+class2 = training[trainingLabel == 2]
+class3 = training[trainingLabel == 3]
+class4 = training[trainingLabel == 4]
+class5 = training[trainingLabel == 5]
+class6 = training[trainingLabel == 6]
+class7 = training[trainingLabel == 7]
+class8 = training[trainingLabel == 8]
+class9 = training[trainingLabel == 9]
+
+# Centroids of classes
+avg0 = np.mean(class0, axis=0)
+avg1 = np.mean(class1, axis=0)
+avg2 = np.mean(class2, axis=0)
+avg3 = np.mean(class3, axis=0)
+avg4 = np.mean(class4, axis=0)
+avg5 = np.mean(class5, axis=0)
+avg6 = np.mean(class6, axis=0)
+avg7 = np.mean(class7, axis=0)
+avg8 = np.mean(class8, axis=0)
+avg9 = np.mean(class9, axis=0)
+
+avg = [avg0,avg1,avg2,avg3,avg4,avg5,avg6,avg7,avg8,avg9]
+
+
+classifierDevLabel = np.zeros(devLabel.shape)
+
+for i in range(0,devLabel.shape[0],1):
+    
+    squareDist = np.zeros(10)
+    
+    for classNb in range(0,10,1):
+        squareDist[classNb] = np.sum(((dev[i] - avg[classNb])**2))
+    
+    classifierDevLabel[i] = np.argmin(squareDist)        
+
+
+errorDev = dev[devLabel != classifierDevLabel]
+error = (errorDev.shape[0]/devLabel.shape[0])*100
+print('Error of classification : ' + str(error) + "%")
---
+++ src/affichage.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+plt.ion()
+
+MAXITER=20
+
+def affiche(W1, W2, A, iteration):
+    cmap = cm.jet
+    gradient=cmap(np.linspace(0.0,1.0,12))
+    # Separe le poids du vecteur normal
+    a0 = A[0]
+    A12 = A[1:3]
+    # Longueur du vecteur normal
+    norm = np.sqrt((A12**2).sum())
+    # Determine le point le plus proche de l'origine
+    H = - a0 * A12 / (norm * norm)
+    # vecteur directeur - rotation de 90deg par produit matriciel
+    # U = np.array([[0, 1], [-1, 0]]) @ A12 / norm
+    U = np.dot( np.array([[0, 1], [-1, 0]]) , A12) / norm
+    # segment autour de H et de longueur 10
+    D = np.vstack((H + 5*U, H - 5*U))
+    # pour l'affichage des points
+    plt.axis([-5, 5, -5, 5])
+    plt.plot(W1[0,:], W1[1,:], 'r+')
+    plt.plot(W2[0,:], W2[1,:], 'bx')
+    plt.plot(D[:,0], D[:,1], ls='-', color=gradient[iteration%(len(gradient))])
+    # plt.show()
